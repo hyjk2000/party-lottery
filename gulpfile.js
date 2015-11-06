7110bb0da30f8cabdcf74fc96f19389072c0147d
James Shih <gtx1024@gmail.com>
2015-11-06 10:41:50 +0800

    Remove unnecessary gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,52 @@
-var gulp = require("gulp");
-var autoprefixer = require("gulp-autoprefixer");
-var babel = require("gulp-babel");
-var concat = require("gulp-concat");
-var sass = require("gulp-sass");
-var sourcemaps = require("gulp-sourcemaps");
-var uglify = require("gulp-uglify");
-var watch = require("gulp-watch");
+'use strict';
 
-gulp.task("default", ["babel", "sass"]);
+let gulp = require('gulp');
+let autoprefixer = require('gulp-autoprefixer');
+let babel = require('gulp-babel');
+let concat = require('gulp-concat');
+let sass = require('gulp-sass');
+let sourcemaps = require('gulp-sourcemaps');
+let uglify = require('gulp-uglify');
 
-var jsFiles = [
-  "src/config.js",
-  "src/names-panel.js",
-  "src/options-panel.js",
-  "src/main.js",
+gulp.task('default', ['babel', 'sass']);
+
+let jsFiles = [
+  'src/config.js',
+  'src/names-panel.js',
+  'src/options-panel.js',
+  'src/main.js'
 ];
 
-gulp.task("babel", function () {
+gulp.task('babel', function () {
   gulp.src(jsFiles)
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(concat("all.js"))
+    .pipe(concat('all.js'))
     .pipe(uglify())
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task("babel:watch", function () {
-  gulp.watch(jsFiles, ["babel"]);
+gulp.task('babel:watch', function () {
+  gulp.watch(jsFiles, ['babel']);
 });
 
-var sassFiles = [
-  "sass/main.scss",
-  "sass/*-panel.scss",
-  "sass/*-theme.scss",
+let sassFiles = [
+  'sass/main.scss',
+  'sass/*-panel.scss',
+  'sass/*-theme.scss'
 ];
 
-gulp.task("sass", function () {
+gulp.task('sass', function () {
   gulp.src(sassFiles)
     .pipe(sourcemaps.init())
-    .pipe(concat("all.css"))
-    .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
+    .pipe(concat('all.css'))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task("sass:watch", function () {
-  gulp.watch(sassFiles, ["sass"]);
+gulp.task('sass:watch', function () {
+  gulp.watch(sassFiles, ['sass']);
 });
