098acd57c9ede8e12d3b0cc28de5ade657659b70
James Shih <gtx1024@gmail.com>
2015-12-04 02:28:27 +0800

    Scripts for starting server

---
+++ bin/server.js
@@ -0,0 +1,45 @@
+#!/usr/local/bin/node
+/*eslint no-console:0*/
+
+'use strict';
+
+let http = require('http');
+let nodeStatic = require('node-static');
+let fileServer = new nodeStatic.Server('.', { gzip: true });
+let webpack = require('webpack');
+let webpackConfig = require('../webpack.config.js');
+
+// Webpack
+
+let compiler = webpack(webpackConfig);
+
+let compilerCallback = (err, stats) => {
+  if (err) return console.error(err);
+  let jsonStats = stats.toJson();
+  if (jsonStats.errors.length > 0) console.error(jsonStats.errors);
+  if (jsonStats.warnings.length > 0) console.warn(jsonStats.warnings);
+}
+
+if (process.env.NODE_ENV == 'production') {
+  compiler.run(compilerCallback);
+} else {
+  compiler.watch({
+      aggregateTimeout: 300,
+      poll: true
+  }, compilerCallback);
+}
+
+// Server
+
+http.createServer((req, res) => {
+    req.addListener('end', () => {
+        fileServer.serve(req, res, (error, result) => {
+          if (error) {
+              res.writeHead(error.status, error.headers);
+              res.end();
+          }
+        });
+    }).resume();
+}).listen(8080);
+
+console.log('Server started on http://localhost:8080/ ...');
