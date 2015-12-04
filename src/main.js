b75637c79ea9ed05a1306c7074f89aeeb7add195
James Shih <gtx1024@gmail.com>
2015-12-04 11:14:57 +0800

    Remove unnecessary things

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-/*global NamesPanel OptionsPanel*/
-
 require('../sass/main.scss');
-import Config from './config.js';
 import NamesPanel from './names-panel.js';
 import OptionsPanel from './options-panel.js';
 
