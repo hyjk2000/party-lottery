b879f8320e2c935e896b020f2e28ba7496b6fa08
James Shih <gtx1024@gmail.com>
2015-12-14 16:04:54 +0800

    Remove unnecessary closures

--- src/config.js
+++ src/config.js
@@ -1,60 +1,56 @@
-let Config = (() => {
-  let instance = null;
+let instance = null;
 
-  const defaults = {
-    names: '',
-    nameRemovedIndexes: [],
-    removeAfterHit: true,
-    stopOnDemand: false,
-    lightTheme: false,
-    readOutNames: false
-  };
+const defaults = {
+  names: '',
+  nameRemovedIndexes: [],
+  removeAfterHit: true,
+  stopOnDemand: false,
+  lightTheme: false,
+  readOutNames: false
+};
 
-  class Config {
-    constructor() {
-      if (instance) return instance;
-      instance = this;
+class Config {
+  constructor() {
+    if (instance) return instance;
+    instance = this;
 
-      for (let prop in defaults) {
-        if (!defaults.hasOwnProperty(prop)) continue;
-        let value = defaults[prop];
-        let loaded = window.localStorage[prop];
-        if (loaded == null || loaded == '') {
-          this[prop] = value;
-        } else {
-          switch (typeof value) {
-            case 'object':
-              this[prop] = loaded.split(',');
-              break;
-            case 'boolean':
-              this[prop] = loaded == '1';
-              break;
-            default:
-              this[prop] = loaded;
-          }
-        }
-      }
-    }
-
-    save() {
-      for (let prop in defaults) {
-        if (!defaults.hasOwnProperty(prop)) continue;
-        let value = defaults[prop];
+    for (let prop in defaults) {
+      if (!defaults.hasOwnProperty(prop)) continue;
+      let value = defaults[prop];
+      let loaded = window.localStorage[prop];
+      if (loaded == null || loaded == '') {
+        this[prop] = value;
+      } else {
         switch (typeof value) {
           case 'object':
-            window.localStorage[prop] = this[prop].join(',');
+            this[prop] = loaded.split(',');
             break;
           case 'boolean':
-            window.localStorage[prop] = this[prop] + 0;
+            this[prop] = loaded == '1';
             break;
           default:
-            window.localStorage[prop] = this[prop]
+            this[prop] = loaded;
         }
       }
     }
   }
 
-  return Config;
-})();
+  save() {
+    for (let prop in defaults) {
+      if (!defaults.hasOwnProperty(prop)) continue;
+      let value = defaults[prop];
+      switch (typeof value) {
+        case 'object':
+          window.localStorage[prop] = this[prop].join(',');
+          break;
+        case 'boolean':
+          window.localStorage[prop] = this[prop] + 0;
+          break;
+        default:
+          window.localStorage[prop] = this[prop]
+      }
+    }
+  }
+}
 
 export default Config;
--- src/options-panel.js
+++ src/options-panel.js
@@ -2,61 +2,57 @@ import Config from './config.js';
 import Nodes from './nodes.js';
 import NamesPanel from './names-panel.js';
 
-let OptionsPanel = (() => {
-  let instance = null;
+let instance = null;
 
-  class OptionsPanel {
-    constructor() {
-      if (instance) return instance;
-      instance = this;
+class OptionsPanel {
+  constructor() {
+    if (instance) return instance;
+    instance = this;
 
-      this.config = new Config();
-      this.namesPanel = new NamesPanel();
+    this.config = new Config();
+    this.namesPanel = new NamesPanel();
 
-      Nodes.optionsBtn.addEventListener('click', e => {
-        this.open();
-      });
+    Nodes.optionsBtn.addEventListener('click', e => {
+      this.open();
+    });
 
-      Nodes.optionsForm.addEventListener('submit', e => {
-        e.preventDefault();
-        this.save();
-        this.namesPanel.init(true);
-        this.close();
-      });
+    Nodes.optionsForm.addEventListener('submit', e => {
+      e.preventDefault();
+      this.save();
+      this.namesPanel.init(true);
+      this.close();
+    });
 
-      Nodes.options_optionCancel.addEventListener('click', e => {
-        this.init();
-        this.close();
-      });
-    }
-
-    init() {
-      Nodes.options_names.value = this.config.names;
-      Nodes.options_removeAfterHit.checked = this.config.removeAfterHit;
-      Nodes.options_stopOnDemand.checked = this.config.stopOnDemand;
-      Nodes.options_lightTheme.checked = this.config.lightTheme;
-      Nodes.options_readOutNames.checked = this.config.readOutNames;
-    }
-
-    save() {
-       this.config.names = Nodes.options_names.value;
-       this.config.removeAfterHit = Nodes.options_removeAfterHit.checked;
-       this.config.stopOnDemand = Nodes.options_stopOnDemand.checked;
-       this.config.lightTheme = Nodes.options_lightTheme.checked;
-       this.config.readOutNames = Nodes.options_readOutNames.checked;
-       this.config.save();
-    }
-
-    open() {
-      Nodes.wrapper.classList.add('flip');
-    }
-
-    close() {
-      Nodes.wrapper.classList.remove('flip');
-    }
+    Nodes.options_optionCancel.addEventListener('click', e => {
+      this.init();
+      this.close();
+    });
   }
 
-  return OptionsPanel;
-})();
+  init() {
+    Nodes.options_names.value = this.config.names;
+    Nodes.options_removeAfterHit.checked = this.config.removeAfterHit;
+    Nodes.options_stopOnDemand.checked = this.config.stopOnDemand;
+    Nodes.options_lightTheme.checked = this.config.lightTheme;
+    Nodes.options_readOutNames.checked = this.config.readOutNames;
+  }
+
+  save() {
+     this.config.names = Nodes.options_names.value;
+     this.config.removeAfterHit = Nodes.options_removeAfterHit.checked;
+     this.config.stopOnDemand = Nodes.options_stopOnDemand.checked;
+     this.config.lightTheme = Nodes.options_lightTheme.checked;
+     this.config.readOutNames = Nodes.options_readOutNames.checked;
+     this.config.save();
+  }
+
+  open() {
+    Nodes.wrapper.classList.add('flip');
+  }
+
+  close() {
+    Nodes.wrapper.classList.remove('flip');
+  }
+}
 
 export default OptionsPanel;
