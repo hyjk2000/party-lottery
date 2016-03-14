75d84b6b9f634e3de4b92fc94c3930faf6e1de82
James Shih <gtx1024@gmail.com>
2016-03-15 01:20:41 +0800

    Deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+DEPLOY_ROOT="$PWD/deploy"
+DEPLOY_TIME=`date +"%Y%m%dT%H%M%S"`
+
+if [ -d "$DEPLOY_ROOT" ]; then
+  rm -rf "$DEPLOY_ROOT"
+fi
+
+git init "$DEPLOY_ROOT"
+
+cp index.prod.html deploy/index.html
+cp apple-touch-icon.png deploy/
+cp favicon.ico deploy/
+cp lottery.appcache deploy/
+cp LICENSE deploy/
+cp -R dist deploy/
+rm -f deploy/dist/*.map
+cp node_modules/babel-polyfill/dist/polyfill.min.js deploy/dist/browser-polyfill.min.js
+
+cd $DEPLOY_ROOT
+
+echo "CACHE MANIFEST" > lottery.appcache
+echo "# Rev.$DEPLOY_TIME" >> lottery.appcache
+find dist/* -print >> lottery.appcache
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://$CI_USER_TOKEN@github.com/hyjk2000/party-lottery.git" master:gh-pages > /dev/null 2>&1
