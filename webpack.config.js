401f7da2c45f098abfd14562e6142df1df5ff3f8
James Shih <hyjk2000@users.noreply.github.com>
2017-01-12 22:55:22 +0800

    Exclude modules for babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader?presets[]=es2015'
       },
       {
